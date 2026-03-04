1f938473e3726181e353d711abb87764edb72368
Yogabars123 <abinaya.m@yogabars.in>
2026-03-04 14:57:00 +0530

    Update 1_RM_Inventory.py

--- pages/1_RM_Inventory.py
+++ pages/1_RM_Inventory.py
@@ -1,10 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from pathlib import Path
 
 st.set_page_config(page_title="RM Inventory", layout="wide")
 
-# Load HTML UI
-with open("../index.html", "r", encoding="utf-8") as f:
-    html = f.read()
+# Get project root folder
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+# HTML file path
+html_file = BASE_DIR / "rm_inventory.html"
+
+# Read HTML
+html = html_file.read_text(encoding="utf-8")
+
+# Display HTML
 components.html(html, height=1200, scrolling=True)
