f82666684c9e9816b12b29eb6e00ffd3e8a2d1fc
Yogabars123 <abinaya.m@yogabars.in>
2026-03-09 15:19:18 +0530

    Update 2_GRN_Data.py

--- pages/2_GRN_Data.py
+++ pages/2_GRN_Data.py
@@ -9,7 +9,7 @@ st.set_page_config(
 )
 
 from pages.Sidebar_style import inject_sidebar
-from pages.data_loader import load_sheet
+from data_loader import load_sheet
 inject_sidebar("GRN Data")
 
 st.markdown("""
